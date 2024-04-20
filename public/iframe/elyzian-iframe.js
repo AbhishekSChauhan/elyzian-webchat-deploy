233c440bd082f5e341438a286021beca795e76e0
AbhishekSChauhan <abhishek.uietpuchd@gmail.com>
2024-04-20 19:19:17 +0530

    add signup pages

--- public/iframe/elyzian-iframe.js
+++ public/iframe/elyzian-iframe.js
@@ -2,7 +2,7 @@
   // create the iframe element
   var iframe = document.createElement('iframe');
   iframe.setAttribute('id', 'chatbot-iframe');
-  iframe.setAttribute('srcdoc', '<!DOCTYPE html><html><head><link href="https://dev.elyzian.in/css/webchat" rel="stylesheet"></head><body><div id="sc-webchat"></div><script defer="" type="text/javascript" src="https://dev.api.elyzian.in/js/webchat/companyId"></script></body></html>');
+  iframe.setAttribute('srcdoc', '<!DOCTYPE html><html><head><link href="https://dev-webchat.elyzian.in/css/webchat" rel="stylesheet"></head><body><div id="sc-webchat"></div><script defer="" type="text/javascript" src="https://dev-webchat.elyzian.in/js/webchat/companyId"></script></body></html>');
   iframe.style.position = 'fixed';
   iframe.style.right = '0px';
   iframe.style.bottom = '0px';
@@ -47,3 +47,54 @@
     chatbotIframe.style.height = '200px';
   }
 })();
+
+(function() {
+  // create the iframe element
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('id', 'signup-iframe');
+  iframe.setAttribute('srcdoc', '<!DOCTYPE html><html><head><link href="https://dev-webchat.elyzian.in/css/signup" rel="stylesheet"></head><body><div id="elyzian-signup"></div><script defer="" type="text/javascript" src="https://dev-webchat.elyzian.in/js/signup/companyId"></script></body></html>');
+  iframe.style.position = 'fixed';
+  iframe.style.right = '0px';
+  iframe.style.bottom = '0px';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.zIndex = '2000000000';
+  iframe.style.border = 'none';
+  
+  // append the iframe to the container div
+  var container = document.getElementById('elyzian-signup-page');
+  container.appendChild(iframe);
+  
+  // add event listener to the window object
+  window.addEventListener('message', function(event) {
+    if (event.data === 'toggle-iframe-size') {
+      toggleIframeSize();
+      console.log('Received toggle-iframe-size message from child iframe');
+    } else if (event.data === 'toggle-normal-iframe-size') {
+      toggleNormalIframeSize();
+      console.log('Received toggle-normal-iframe-size message from child iframe');
+    } else if(event.data === 'close-on-click-outside') {
+      console.log('Received close-on-click-outside message from child iframe====');
+    }
+  });
+  
+  // toggle the iframe size
+  function toggleIframeSize() {
+    var signupIframe = document.getElementById('signup-iframe');
+    if (window.innerWidth < 600) {
+      signupIframe.style.width = '100%';
+      signupIframe.style.height = '100%';
+    } else {
+      signupIframe.style.width = '100%';
+      signupIframe.style.height = '100%'; 
+    }
+  }
+  
+  // set the iframe size to normal
+  function toggleNormalIframeSize() {
+    var signupIframe = document.getElementById('signup-iframe');
+    signupIframe.style.width = '0px';
+    signupIframe.style.height = '0px';
+  }
+})();
+
--- server.js
+++ server.js
@@ -7,11 +7,16 @@ const app = express();
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/',(req,res)=>{
+  console.log('coming to root');
+  res.status(200).send('Succeses');
+})
 // Route to serve the minified JS file
 app.get('/js/webchat/:companyId', (req, res) => {
   const companyId = req.params.companyId;
   const filePath = path.join(__dirname, 'public', 'js', 'webchat.js');
-
+   console.log('in company id', companyId);
   // Check if the file exists
   if (fs.existsSync(filePath)) {
     // Read the file
@@ -23,7 +28,7 @@ app.get('/js/webchat/:companyId', (req, res) => {
       }
 
       // Replace desired values with companyId
-      const modifiedData = data.replace(/650c64be035ef7dd6769dbc1/g, companyId);
+      const modifiedData = data.replace(/650c64be035ef7dd6769dbc0/g, companyId);
 
       // Send the modified content as the response
       res.type('text/javascript').send(modifiedData);
@@ -37,6 +42,52 @@ app.get('/js/webchat/:companyId', (req, res) => {
 app.get('/css/webchat', (req, res) => {
   // const filename = req.params.filename;
   const filePath = path.join(__dirname, 'public', 'css', 'webchat.css');
+
+  // Check if the file exists
+  if (fs.existsSync(filePath)) {
+    // Read the file and send it as a response
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        res.type('text/css').send(data);
+      }
+    });
+  } else {
+    res.status(404).send('File Not Found');
+  }
+});
+
+app.get('/js/signup/:companyId', (req, res) => {
+  const companyId = req.params.companyId;
+  const filePath = path.join(__dirname, 'public', 'js', 'signup.js');
+   console.log('in company id', companyId);
+  // Check if the file exists
+  if (fs.existsSync(filePath)) {
+    // Read the file
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading file:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      // Replace desired values with companyId
+      const modifiedData = data.replace(/650c64be035ef7dd6769dbc0/g, companyId);
+
+      // Send the modified content as the response
+      res.type('text/javascript').send(modifiedData);
+    });
+  } else {
+    res.status(404).send('File Not Found');
+  }
+});
+
+// Route to serve the minified CSS file
+app.get('/css/signup', (req, res) => {
+  // const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'public', 'css', 'signup.css');
 
   // Check if the file exists
   if (fs.existsSync(filePath)) {

19840ee10c1e1a5ccc41572ed957de849b91beb3
AbhishekSChauhan <abhishek.uietpuchd@gmail.com>
2024-05-17 12:01:37 +0530

    fix 10 sec signup page delay

--- public/iframe/elyzian-iframe.js
+++ public/iframe/elyzian-iframe.js
@@ -1,3 +1,98 @@
+// (function() {
+//   // create the iframe element
+//   var iframe = document.createElement('iframe');
+//   iframe.setAttribute('id', 'chatbot-iframe');
+//   iframe.setAttribute('srcdoc', '<!DOCTYPE html><html><head><link href="https://dev-webchat.elyzian.in/css/webchat" rel="stylesheet"></head><body><div id="sc-webchat"></div><script defer="" type="text/javascript" src="https://dev-webchat.elyzian.in/js/webchat/companyId"></script></body></html>');
+//   iframe.style.position = 'fixed';
+//   iframe.style.right = '0px';
+//   iframe.style.bottom = '0px';
+//   iframe.style.width = '336px';
+//   iframe.style.height = '320px';
+//   iframe.style.zIndex = '2147483001';
+//   iframe.style.border = 'none';
+  
+//   // append the iframe to the container div
+//   var container = document.getElementById('salescaptain-webchat');
+//   container.appendChild(iframe);
+  
+//   // add event listener to the window object
+//   window.addEventListener('message', function(event) {
+//     if (event.data === 'toggle-iframe-size') {
+//       toggleIframeSize();
+//       console.log('Received toggle-iframe-size message from child iframe');
+//     } else if (event.data === 'toggle-normal-iframe-size') {
+//       toggleNormalIframeSize();
+//       console.log('Received toggle-normal-iframe-size message from child iframe');
+//     } else if(event.data === 'close-on-click-outside') {
+//       console.log('Received close-on-click-outside message from child iframe====');
+//     }
+//   });
+  
+//   // toggle the iframe size
+//   function toggleIframeSize() {
+//     var chatbotIframe = document.getElementById('chatbot-iframe');
+//     if (window.innerWidth < 600) {
+//       chatbotIframe.style.width = '100%';
+//       chatbotIframe.style.height = '100%';
+//     } else {
+//       chatbotIframe.style.width = '100%';
+//       chatbotIframe.style.height = '100%'; 
+//     }
+//   }
+  
+//   // set the iframe size to normal
+//   function toggleNormalIframeSize() {
+//     var chatbotIframe = document.getElementById('chatbot-iframe');
+//     // chatbotIframe.style.width = '300px';
+//     // chatbotIframe.style.height = '200px';
+//     chatbotIframe.style.width = '336px';
+//     chatbotIframe.style.height = '320px';
+//   }
+// })();
+
+// (function() {
+//   // create the iframe element
+//   var iframe = document.createElement('iframe');
+//   iframe.setAttribute('id', 'signup-iframe');
+//   iframe.setAttribute('srcdoc', '<!DOCTYPE html><html><head><link href="https://dev-webchat.elyzian.in/css/signup" rel="stylesheet"></head><body><div id="elyzian-signup"></div><script defer="" type="text/javascript" src="https://dev-webchat.elyzian.in/js/signup/companyId"></script></body></html>');
+//   iframe.style.position = 'fixed';
+//   iframe.style.right = '0px';
+//   iframe.style.bottom = '0px';
+//   iframe.style.width = '100%';
+//   iframe.style.height = '100%';
+//   iframe.style.zIndex = '2147483001';
+//   iframe.style.border = 'none';
+  
+//   // append the iframe to the container div
+//   var container = document.getElementById('elyzian-signup-page');
+//   container.appendChild(iframe);
+  
+//   // add event listener to the window object
+//   window.addEventListener('message', function(event) {
+//     if (event.data === 'close-signup-iframe') {
+//       closeSignupIframe();
+//       console.log('Received close-signup-iframe message from child iframe(i.e. signup component)');
+//     } else if (event.data === 'open-signup-iframe') {
+//       openSingupIframe();
+//       console.log('Received toggle-normal-iframe-size message from child iframe');
+//     }
+//   });
+  
+//   // toggle the iframe size
+//   function closeSignupIframe() {
+//     var signupIframe = document.getElementById('signup-iframe');
+//     signupIframe.style.width = '0px';
+//     signupIframe.style.height = '0px'; 
+//   }
+  
+//   // set the iframe size to normal
+//   function openSingupIframe() {
+//     var signupIframe = document.getElementById('signup-iframe');
+//     signupIframe.style.width = '100%';
+//     signupIframe.style.height = '100%';
+//   }
+// })();
+
 (function() {
   // create the iframe element
   var iframe = document.createElement('iframe');
@@ -43,52 +138,53 @@
   // set the iframe size to normal
   function toggleNormalIframeSize() {
     var chatbotIframe = document.getElementById('chatbot-iframe');
-    // chatbotIframe.style.width = '300px';
-    // chatbotIframe.style.height = '200px';
     chatbotIframe.style.width = '336px';
     chatbotIframe.style.height = '320px';
   }
 })();
 
-(function() {
-  // create the iframe element
-  var iframe = document.createElement('iframe');
-  iframe.setAttribute('id', 'signup-iframe');
-  iframe.setAttribute('srcdoc', '<!DOCTYPE html><html><head><link href="https://dev-webchat.elyzian.in/css/signup" rel="stylesheet"></head><body><div id="elyzian-signup"></div><script defer="" type="text/javascript" src="https://dev-webchat.elyzian.in/js/signup/companyId"></script></body></html>');
-  iframe.style.position = 'fixed';
-  iframe.style.right = '0px';
-  iframe.style.bottom = '0px';
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
-  iframe.style.zIndex = '2147483001';
-  iframe.style.border = 'none';
-  
-  // append the iframe to the container div
-  var container = document.getElementById('elyzian-signup-page');
-  container.appendChild(iframe);
-  
-  // add event listener to the window object
-  window.addEventListener('message', function(event) {
-    if (event.data === 'close-signup-iframe') {
-      closeSignupIframe();
-      console.log('Received close-signup-iframe message from child iframe(i.e. signup component)');
-    } else if (event.data === 'open-signup-iframe') {
-      openSingupIframe();
-      console.log('Received toggle-normal-iframe-size message from child iframe');
+// Delay the execution of the second IIFE by 10 seconds
+setTimeout(function() {
+  (function() {
+    // create the iframe element
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('id', 'signup-iframe');
+    iframe.setAttribute('srcdoc', '<!DOCTYPE html><html><head><link href="https://dev-webchat.elyzian.in/css/signup" rel="stylesheet"></head><body><div id="elyzian-signup"></div><script defer="" type="text/javascript" src="https://dev-webchat.elyzian.in/js/signup/companyId"></script></body></html>');
+    iframe.style.position = 'fixed';
+    iframe.style.right = '0px';
+    iframe.style.bottom = '0px';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.zIndex = '2147483001';
+    iframe.style.border = 'none';
+    
+    // append the iframe to the container div
+    var container = document.getElementById('elyzian-signup-page');
+    container.appendChild(iframe);
+    
+    // add event listener to the window object
+    window.addEventListener('message', function(event) {
+      if (event.data === 'close-signup-iframe') {
+        closeSignupIframe();
+        console.log('Received close-signup-iframe message from child iframe(i.e. signup component)');
+      } else if (event.data === 'open-signup-iframe') {
+        openSignupIframe();
+        console.log('Received toggle-normal-iframe-size message from child iframe');
+      }
+    });
+    
+    // toggle the iframe size
+    function closeSignupIframe() {
+      var signupIframe = document.getElementById('signup-iframe');
+      signupIframe.style.width = '0px';
+      signupIframe.style.height = '0px'; 
     }
-  });
-  
-  // toggle the iframe size
-  function closeSignupIframe() {
-    var signupIframe = document.getElementById('signup-iframe');
-    signupIframe.style.width = '0px';
-    signupIframe.style.height = '0px'; 
-  }
-  
-  // set the iframe size to normal
-  function openSingupIframe() {
-    var signupIframe = document.getElementById('signup-iframe');
-    signupIframe.style.width = '100%';
-    signupIframe.style.height = '100%';
-  }
-})();
+    
+    // set the iframe size to normal
+    function openSignupIframe() {
+      var signupIframe = document.getElementById('signup-iframe');
+      signupIframe.style.width = '100%';
+      signupIframe.style.height = '100%';
+    }
+  })();
+}, 10000); // 10000 milliseconds = 10 seconds
